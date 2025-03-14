7077a8e97f1b65f15c0f16e9f3818c43092e1c46
KuragariDemo <demomello110078@gmail.com>
2025-03-14 13:53:06 +0700

    Excluded .vs folder from Git

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using LibraryManagement.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Register DbContext with Oracle Connection
+builder.Services.AddDbContext<LibraryContext>(options =>
+    options.UseOracle(builder.Configuration.GetConnectionString("OracleDbContext")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
